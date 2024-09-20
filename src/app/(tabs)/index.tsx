bf468557c3cfabdc5c08a47432f488cc63ec71a2
amir <amir.tamim@gatech.edu>
2024-09-20 14:56:38 -0700

    add new workout button

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
+import {Text, View, StyleSheet, Pressable} from "react-native";
+
+
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Enter sets and reps for max gains!</Text>
+        <Pressable style={styles.button}>
+            <Text style={styles.plusSign}>+</Text>
+        </Pressable>
+      <Text style={styles.title}>Add New Workout Day</Text>
     </View>
   );
 }
@@ -14,6 +19,28 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
-        justifyContent: "center",
-    }
+        justifyContent: "center"
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: '600',
+        marginTop: 20
+    },
+
+    button: {
+        backgroundColor: 'green',    // Green background for the button
+        borderRadius: 50,            // Round button
+        width: 60,                   // Width of the button
+        height: 60,                  // Height of the button
+        justifyContent: 'center',    // Center the plus sign vertically
+        alignItems: 'center',
+
+    },
+
+    plusSign:{
+        fontSize: 30,
+        color: 'white',
+    },
+
+
 })
