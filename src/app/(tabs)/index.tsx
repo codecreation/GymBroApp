f7c1da6d8ed0a902e324f5e21b52ff88e4f3b512
amir <amir.tamim@gatech.edu>
2024-09-20 14:11:53 -0700

    add StyleSheet

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>Enter sets and reps for max gains!</Text>
     </View>
   );
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    }
+})
